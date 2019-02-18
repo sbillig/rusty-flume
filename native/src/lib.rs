ee51c9ebec10dc9ed534f32c3b0c2ae66785225f
sean billig <sean.billig@gmail.com>
2019-02-18 10:27:55 -0600

    Support batch append, reverse streams

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,14 +2,19 @@
 extern crate neon;
 extern crate flumedb;
 
-use flumedb::{FlumeLog, LogEntry, OffsetLog, OffsetLogIter};
+use flumedb::{BidirIterator, FlumeLog, LogEntry, OffsetLog, OffsetLogIter};
 use neon::prelude::*;
 use std::collections::HashMap;
 
 pub struct SyncLog {
     log: flumedb::OffsetLog<u32>,
-    streams: HashMap<u32, OffsetLogIter<u32>>,
+    streams: HashMap<u32, Stream>,
     next_id: u32
+}
+
+struct Stream {
+    iter: OffsetLogIter<u32>,
+    reverse: bool
 }
 
 declare_types! {
@@ -18,10 +23,23 @@ declare_types! {
             let path = cx.argument::<JsString>(0)?;
 
             Ok(SyncLog {
-                log: OffsetLog::new(path.value()),
+                log: OffsetLog::new(path.value()).unwrap(),
                 streams: HashMap::new(),
                 next_id: 0
             })
+        }
+
+        method last_offset(mut cx) {
+            let n = {
+                let mut this = cx.this();
+                let g = cx.lock();
+                let mut t = this.borrow_mut(&g);
+                match t.log.latest() {
+                    Some(n) => n as i64,
+                    None    => -1
+                }
+            };
+            Ok(cx.number(n as f64).upcast())
         }
 
         method get(mut cx) {
@@ -56,18 +74,45 @@ declare_types! {
             Ok(cx.number(r as f64).upcast())
         }
 
+        method append_batch(mut cx) {
+            // TODO: Should probably use t.log.append_batch
+            let bufarray = cx.argument::<JsArray>(0)?;
+            let bufvec = bufarray.to_vec(&mut cx)?;
+
+            let r = {
+                let mut this = cx.this();
+                let g = cx.lock();
+                let mut t = this.borrow_mut(&g);
+
+                let mut offset = 0;
+                for h in bufvec.iter() {
+                    let buf = h.downcast::<JsBuffer>().unwrap();
+                    let data = buf.borrow(&g);
+                    offset = t.log.append(data.as_slice::<u8>()).unwrap();
+                }
+                offset
+            };
+            Ok(cx.number(r as f64).upcast())
+        }
+
+
         method create_stream(mut cx) {
+            let reverse = cx.argument::<JsBoolean>(0)?.value();
+
             let stream_id = {
                 let mut this = cx.this();
                 let g = cx.lock();
-
                 let mut t = this.borrow_mut(&g);
 
                 let id = t.next_id;
                 t.next_id += 1;
-                let iter = t.log.iter();
+                let iter = if reverse {
+                    t.log.iter_at_offset(t.log.end())
+                } else {
+                    t.log.iter()
+                };
 
-                t.streams.insert(id, iter);
+                t.streams.insert(id, Stream { iter, reverse });
                 id
             };
 
@@ -89,22 +134,33 @@ declare_types! {
 
         method read_from_stream(mut cx) {
             let id = cx.argument::<JsNumber>(0)?.value() as u32;
-            let count = cx.argument::<JsNumber>(1)?.value() as u32;
+            let count = cx.argument::<JsNumber>(1)?.value() as usize;
 
-            let out = JsArray::new(&mut cx, count);
+            // TODO: don't need this vec
             let entries: Vec<LogEntry> = {
                 let mut this = cx.this();
                 let g = cx.lock();
                 let mut t = this.borrow_mut(&g);
-                t.streams.get_mut(&id).map(|iter| iter.take(count as usize).collect()).unwrap()
+                let mut stream = t.streams.get_mut(&id).unwrap();
+                if stream.reverse {
+                    stream.iter.backward().take(count).collect()
+                } else {
+                    stream.iter.forward().take(count).collect()
+                }
             };
+
+            let out = JsArray::new(&mut cx, entries.len() as u32);
             for (i, e) in entries.iter().enumerate() {
-                let mut buf = JsBuffer::new(&mut cx, e.data_buffer.len() as u32).unwrap();
+                let mut buf = cx.buffer(e.data.len() as u32).unwrap();
                 cx.borrow_mut(&mut buf, |data| {
                     let slice = data.as_mut_slice::<u8>();
-                    slice.copy_from_slice(&e.data_buffer);
+                    slice.copy_from_slice(&e.data);
                 });
-                out.set(&mut cx, i as u32, buf)?;
+                let seq = cx.number(e.offset as f64);
+                let entry = cx.empty_object();
+                entry.set(&mut cx, "value", buf)?;
+                entry.set(&mut cx, "seq", seq)?;
+                out.set(&mut cx, i as u32, entry)?;
             }
 
             Ok(out.upcast())
--- test/index.js
+++ test/index.js
@@ -1,3 +1,62 @@
-require('test-flumelog')(require('../'), function () {
-  console.log('done')
+let test = require('tape')
+let run_flumelog_tests = require('test-flumelog')
+let OffsetLog = require('../').OffsetLog
+
+function tmpfile() {
+  return '/tmp/offset-test_'+Date.now()+'.log'
+}
+
+test('basics', (t) => {
+  let log = OffsetLog(tmpfile(), {})
+  t.equal(log.since.value, -1, 'empty log .since.value == -1')
+
+  log.append(Buffer.from('abc'), (err, offset) => {
+    t.equal(offset, 0, 'initial offset is 0')
+
+    log.append(Buffer.from('12345'), (err, offset) => {
+      t.equal(offset, 15, 'next offset') // three u32s + "abc"
+      t.equal(log.since.value, 15, '.since.value is latest offset')
+
+      log.get(15, function(err, buf) {
+        t.equal(buf.toString('utf8'), '12345', 'get second record')
+      })
+
+      let s = log.stream()
+      s(false, (err, entry) => {
+        t.equal(entry.seq, 0, 'stream 0 seq')
+        t.equal(entry.value.toString('utf8'), 'abc', 'stream 0 value')
+
+        s(false, (err, entry) => {
+          t.equal(entry.seq, 15, 'stream 1 seq')
+          t.equal(entry.value.toString('utf8'), '12345', 'stream 1')
+
+          s(false, (err, entry) => {
+            t.equal(err, true, 'stream end')
+            t.end()
+          })
+        })
+      })
+    })
+  })
 })
+
+test('foo', (t) => {
+  let log = OffsetLog(tmpfile(), {})
+  log.append(['abc', 'def', '123', '456'], (err, offset) => {
+    t.equal(offset, 45, 'append batch')
+
+    let s = log.stream({reverse: true, values: false})
+    s(false, (err, seq) => {
+      t.equal(seq, 45, 'reverse stream')
+      s(false, (err, seq) => {
+        t.equal(seq, 30, 'reverse stream')
+        t.end()
+      })
+    })
+
+  })
+})
+
+
+// run_flumelog_tests(() => OffsetLog(tmpfile(), {}),
+//                    () => console.log('done'))
